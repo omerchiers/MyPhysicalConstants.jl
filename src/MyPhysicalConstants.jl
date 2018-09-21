2100f542cb3915fa3b7e314867d6c457e442bf86
Olivier Merchiers <omerchiers@gmail.com>
2018-09-21 16:41:10 +0200

    created Package for my physical constants

--- src/MyPhysicalConstants.jl
+++ src/MyPhysicalConstants.jl
@@ -1,5 +1,30 @@
 module MyPhysicalConstants
 
-# package code goes here
+# exporting constants
+export c0,
+       sigma,
+       h,
+       ħ,
+       kb,
+       vf_au
+
+# Physical constants
+
+" Speed of light in vacuum in m/s "
+const c0    = 299792458
+
+" Stefan-Boltzmann's constant in W m^{-2} K^{-4} "
+const sigma = 5.670367e-8
+
+" Planck's constant in J s "
+const h = 6.6260704e-34
+const ħ = 1.0545718e-34
+
+" Boltzmann constant in m^2 kg s^{-2} K^{-1} "
+const kb = 1.38064852e-23
+
+" Fermi velocity for gold in rad m s^{-1} "
+const vf_au = 1.4e6*2.0*pi
+
 
 end # module
